e4fdd84549b25e77f60904a324df5571ed401861
ouyangluwei(riseunion) <ouyangluwei@riseunion.io>
2026-06-23 19:33:54 +0800

    feat: resolve effective vnpu mode from node when pod annotation absent
    Signed-off-by: ouyangluwei(riseunion) <ouyangluwei@riseunion.io>

--- internal/server/allocate.go
+++ internal/server/allocate.go
@@ -66,8 +66,9 @@ func (ps *PluginServer) buildContainerAllocateResponse(pod *v1.Pod, ctrName stri
 	resp.Envs["ASCEND_VISIBLE_DEVICES"] = ascendVisibleDevices
 
 	vnpuMode := pod.Annotations[VNPUModeAnnotation]
-	klog.V(4).Infof("Pod %s vnpu mode: %s", pod.Name, vnpuMode)
-	if vnpuMode == VNPUModeHamiCore {
+	effectiveHamiCore := vnpuMode == VNPUModeHamiCore || (vnpuMode == "" && ps.mgr.IsHamiVnpuCore())
+	klog.V(4).Infof("Pod %s vnpu mode: %q, effectiveHamiCore: %v", pod.Name, vnpuMode, effectiveHamiCore)
+	if effectiveHamiCore {
 		// 1. Handle volume mount injection
 		var mounts []*v1beta1.Mount
 		// A.Huawei driver and SMI toolchain (Read-Only)
